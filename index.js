3bd77d26bef2177cac4dd34c9757036865118a0b
arefweb <arefmovahedz@gmail.com>
2022-12-29 23:20:20 +0330

    First Commit

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use((err, req, res) => {
   const statusCode = err.statusCode || 500;
   console.error(err.message, err.stack);
   res.status(statusCode).json({ message: err.message });
-  
+
 });
 
 app.use(
